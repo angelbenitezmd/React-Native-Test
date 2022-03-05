fb0eab2b64a68ec5c24485831ed6a8f182aabe6a
Angel Benitez <angelbenitez@gmail.com>
2022-03-04 20:48:29 -0500

    Activity Indicator

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,19 +5,22 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import Constants from "expo-constants";
-import {
-  View,
-  Platform,
-  StyleSheet,
-  Text,
-  ScrollView,
-  Image,
-} from "react-native";
+import { View, Platform, StyleSheet, Text, ScrollView, Image} from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Icon } from "react-native-elements";
 import SafeAreaView from "react-native-safe-area-view";
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions,
+    fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -203,6 +206,14 @@ const MainNavigator = createDrawerNavigator(
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+
+  componentDidMount() {
+      this.props.fetchCampsites();
+      this.props.fetchComments();
+      this.props.fetchPromotions();
+      this.props.fetchPartners();
+  }
+
   render() {
     return (
       <View
@@ -246,4 +257,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
